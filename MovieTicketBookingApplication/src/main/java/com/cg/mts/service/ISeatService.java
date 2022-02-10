9f3a55247aa639b5f87c5b946d6323a176f8e573
Deekshitha <62751775+deekshithapallapothu@users.noreply.github.com>
2022-02-10 19:34:31 +0530

    Delete MovieTicketBookingApplication directory

--- MovieTicketBookingApplication/src/main/java/com/cg/mts/service/ISeatService.java
+++
@@ -1,9 +0,0 @@
-package com.cg.mts.service;
-
-import com.cg.mts.entities.Seats;
-
-public interface ISeatService {
-public Seats bookSeat(Seats seats);
-public Seats cancelSeatBooking(Seats seats);
-
-}
