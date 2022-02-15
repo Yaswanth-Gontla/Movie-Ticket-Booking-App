30d8a34d803fbf2093342b5a57e9d3fa78fefbcb
Mohith-Bolla123 <99387571+Mohith-Bolla123@users.noreply.github.com>
2022-02-15 14:32:40 +0530

    Add files via upload

---
+++ Movie-Ticket-Booking-App-main/MovieTicketBookingApplication/src/main/java/com/cg/mts/repository/CustomerRepository.java
@@ -0,0 +1,19 @@
+package com.cg.mts.repository;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.cg.mts.entities.Customers;
+
+@Repository
+public interface CustomerRepository {
+	public Customers getCustomerByName(String customerName);
+	public Customers getCustomersById(int customerId);
+
+	
+
+
+
+}
